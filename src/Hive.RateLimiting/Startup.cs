a0a98184ebc9d89a3e318f3a4b164be9de8b32bb
Caeden117 <caeden.s@outlook.com>
2021-07-24 18:31:54 -0700

    Add rate limiting plugin

---
+++ src/Hive.RateLimiting/Startup.cs
@@ -0,0 +1,36 @@
+﻿using AspNetCoreRateLimit;
+using Hive.Plugins;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hive.RateLimiting
+{
+    [PluginStartup]
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+            => Configuration = config;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            _ = services.AddMemoryCache()
+                .Configure<ClientRateLimitOptions>(Configuration.GetSection("ClientRateLimiting"))
+                .Configure<ClientRateLimitPolicies>(Configuration.GetSection("ClientRateLimitPolicies"))
+                .Configure<IpRateLimitOptions>(Configuration.GetSection("IpRateLimiting"))
+                .Configure<IpRateLimitPolicies>(Configuration.GetSection("IpRateLimitPolicies"))
+                .AddSingleton<IClientPolicyStore, MemoryCacheClientPolicyStore>()
+                .AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>()
+                .AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>()
+                .AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+        }
+
+        public static void Configure(IApplicationBuilder app)
+        {
+            _ = app.UseClientRateLimiting()
+                .UseIpRateLimiting();
+        }
+    }
+}
