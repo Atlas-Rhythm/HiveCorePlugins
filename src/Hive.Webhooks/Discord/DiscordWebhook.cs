a886e94098adb8d4c6de3517ba06bc5a8f9278e4
Auros Nexus <aurosnex@gmail.com>
2021-08-21 15:17:54 -0400

    Fix Discord Webhook case

--- src/Hive.Webhooks/Discord/DiscordWebhook.cs
+++ src/Hive.Webhooks/Discord/DiscordWebhook.cs
@@ -4,7 +4,7 @@ using System.Text.Json.Serialization;
 
 namespace Hive.Webhooks.Discord
 {
-    internal class DiscordWebHook
+    internal class DiscordWebhook
     {
         public string? Content { get; }
         public List<DiscordEmbed>? Embeds { get; }
@@ -13,14 +13,14 @@ namespace Hive.Webhooks.Discord
         public Uri? AvatarURL { get; set; }
         public string? Username { get; set; }
 
-        public DiscordWebHook(string content, string? username = null, Uri? avatarURL = null)
+        public DiscordWebhook(string content, string? username = null, Uri? avatarURL = null)
         {
             Content = content;
             Username = username;
             AvatarURL = avatarURL;
         }
 
-        public DiscordWebHook(List<DiscordEmbed> embeds, string? username = null, Uri? avatarURL = null)
+        public DiscordWebhook(List<DiscordEmbed> embeds, string? username = null, Uri? avatarURL = null)
         {
             Embeds = embeds;
             Username = username;
