2ba2fa408d11004c78a1d2adf08a877c8188cdf6
Auros Nexus <aurosnex@gmail.com>
2021-08-24 10:40:17 -0400

    When Writing Null

--- src/Hive.Webhooks/Discord/DiscordEmbed.cs
+++ src/Hive.Webhooks/Discord/DiscordEmbed.cs
@@ -7,18 +7,39 @@ namespace Hive.Webhooks.Discord
     /// <see cref="https://discord.com/developers/docs/resources/channel#embed-object"/>
     public class DiscordEmbed
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Title { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Description { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public Uri? URL { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public DateTime? Timestamp { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public int? Color { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public DiscordFooter? Footer { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public DiscordMedia? Image { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public DiscordMedia? Thumbnail { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public DiscordMedia? Video { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public DiscordProvider? Provider { get; set; }
+
 #pragma warning disable CA1002 // Do not expose generic lists
 #pragma warning disable CA2227 // Collection properties should be read only
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public List<DiscordField> Fields { get; set; } = new();
 #pragma warning restore CA2227 // Collection properties should be read only
 #pragma warning restore CA1002 // Do not expose generic lists
@@ -28,9 +49,11 @@ namespace Hive.Webhooks.Discord
     {
         public string Text { get; set; } = null!;
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [JsonPropertyName("icon_url")]
         public Uri? IconURL { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [JsonPropertyName("proxy_icon_url")]
         public Uri? ProxyIconURL { get; set; }
     }
@@ -39,6 +62,8 @@ namespace Hive.Webhooks.Discord
     {
         public string Name { get; set; } = null!;
         public string Value { get; set; } = null!;
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public bool? Inline { get; set; }
 
         public DiscordField(string name, string value)
@@ -50,29 +75,44 @@ namespace Hive.Webhooks.Discord
 
     public class DiscordMedia
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [JsonPropertyName("proxy_url")]
         public Uri? ProxyURL { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public Uri? URL { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public int? Height { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public int? Width { get; set; }
     }
 
     public class DiscordProvider
     {
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Name { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public Uri? URL { get; set; }
     }
 
     public class DiscordAuthor
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [JsonPropertyName("proxy_icon_url")]
         public Uri? ProxyIconURL { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [JsonPropertyName("icon_url")]
         public Uri? IconURL { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Name { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public Uri? URL { get; set; }
     }
 }
