ec3d0c17b4c29bea85a539b4e56bde619691f0e1
Auros Nexus <aurosnex@gmail.com>
2021-10-09 08:53:57 -0400

    Visual Studio is actually insane

--- src/Hive.Webhooks/Startup.cs
+++ src/Hive.Webhooks/Startup.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using Hive.Plugins;
+using Microsoft.Extensions.Configuration;
 
 namespace Hive.Webhooks
 {
-    public class PluginStartup
+    [PluginStartup]
+    public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+            => Configuration = config;
+
     }
 }
