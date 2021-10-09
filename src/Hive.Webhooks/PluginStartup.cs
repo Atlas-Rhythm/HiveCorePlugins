a07c205d134b846907ec07b356c2f0c375d750be
Auros Nexus <aurosnex@gmail.com>
2021-10-09 08:19:47 -0400

    New project

---
+++ src/Hive.Webhooks/PluginStartup.cs
@@ -0,0 +1,14 @@
+﻿using Hive.Plugins;
+using Microsoft.Extensions.Configuration;
+
+namespace Hive.Webhooks
+{
+    [PluginStartup]
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+            => Configuration = config;
+    }
+}
