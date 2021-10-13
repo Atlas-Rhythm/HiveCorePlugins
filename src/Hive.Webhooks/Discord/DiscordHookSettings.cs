e21a4525cd2597eac32bc2530d1f8dc944ffec4e
Auros Nexus <aurosnex@gmail.com>
2021-10-13 05:27:42 -0400

    We won't worry about separate hook plugins until I ask Danike again

---
+++ src/Hive.Webhooks/Discord/DiscordHookSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Hive.Webhooks.Discord
+{
+    // TODO
+    internal class DiscordHookSettings
+    {
+    }
+}
---
+++ src/Hive.Webhooks/Discord/DiscordWebhook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Hive.Webhooks.Discord
+{
+    internal class DiscordWebhook
+    {
+        public string? Content { get; }
+        public List<DiscordEmbed>? Embeds { get; }
+
+        [JsonPropertyName("avatar_url")]
+        public Uri? AvatarURL { get; set; }
+        public string? Username { get; set; }
+
+        public DiscordWebhook(string content, string? username = null, Uri? avatarURL = null)
+        {
+            Content = content;
+            Username = username;
+            AvatarURL = avatarURL;
+        }
+
+        public DiscordWebhook(List<DiscordEmbed> embeds, string? username = null, Uri? avatarURL = null)
+        {
+            Embeds = embeds;
+            Username = username;
+            AvatarURL = avatarURL;
+        }
+
+        public DiscordWebhook(DiscordEmbed embed, string? username = null, Uri? avatarURL = null)
+        {
+            Embeds = new List<DiscordEmbed>();
+
+            Embeds.Add(embed);
+            Username = username;
+            AvatarURL = avatarURL;
+        }
+    }
+}
