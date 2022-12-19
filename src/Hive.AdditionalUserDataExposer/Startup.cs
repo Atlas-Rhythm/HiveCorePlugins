81b128457ab22a7556691381c5d9d527ab0a7a64
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-12-19 00:13:13 -0500

    Created AdditionalUserDataExposerPlugin

---
+++ src/Hive.AdditionalUserDataExposer/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Hive.Controllers;
+using Hive.Plugins;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hive.AdditionalUserDataExposer
+{
+    [PluginStartup]
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+            => Configuration = config;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            _ = services
+                .AddSingleton<IUserPlugin, AdditionalUserDataExposerPlugin>()
+                .AddOptions<AdditionalUserDataExposerOptions>().Bind(Configuration);
+        }
+    }
+}
